0bdd1db01ed5a563cf0110806ec5b835ed7331b3
Linh Do <linh.carlynda@gmail.com>
2019-10-31 11:09:23 -0700

    Finished!!!

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-var rollDice, newGame, holdPoint, diceImg, winPoint; 
+var rollDice, newGame, holdPoint, diceImg, winPoint, gamePlay; 
 var scores = []; 
 var currentScore; 
 var activePlayer; 
-
-activePlayer = 0; 
-currentScore = 0; 
-winPoint = 20;
-scores = [0,0]; 
 
 btnRoll = document.querySelector('.btn-roll'); 
 btnNew = document.querySelector('.btn-new'); 
@@ -15,50 +10,50 @@ diceImg = document.querySelector('.dice');
 playerPan0 = document.querySelector('.player-panel-0'); 
 playerPan1 = document.querySelector('.player-panel-1'); 
 
-document.querySelector('.score-0').textContent = '0';
-document.querySelector('.score-1').textContent = '0';
-document.querySelector('#current-0').textContent = '0';
-document.querySelector('#current-1').textContent = '0';
-diceImg.style.display = 'none'; 
+init()
+
 
 
 btnRoll.addEventListener('click', function(){
-
-    //show randome dice
-    var randNum = Math.floor(Math.random() * 6 + 1); 
-    diceImg.style.display = 'block';
-    diceImg.src="./img/dice-" + randNum  + ".png"; 
-    
-    if (randNum !== 1){
-        //add value of random dice to current score
-        currentScore += randNum; 
-        document.querySelector('#current-' + activePlayer).textContent = currentScore;
-    } else{
-        nextPlayer();
+    if(gamePlay){
+        //show randome dice
+        var randNum = Math.floor(Math.random() * 6 + 1); 
+        diceImg.style.display = 'block';
+        diceImg.src="./img/dice-" + randNum  + ".png"; 
+        
+        if (randNum !== 1){
+            //add value of random dice to current score
+            currentScore += randNum; 
+            document.querySelector('#current-' + activePlayer).textContent = currentScore;
+        } else{
+            nextPlayer();
+        }
+        //active player changes 
+        //current score becomes 0
     }
-    //active player changes 
-    //current score becomes 0
     
 }); 
 
 btnHold.addEventListener('click', function(){
-
-    scores[activePlayer] += currentScore; 
-
-    document.querySelector('.score-' + activePlayer).textContent = scores[activePlayer]; 
-
-    if(scores[activePlayer] >= winPoint){
-        
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.player-panel-' + activePlayer).classList.add('.winner'); 
-        document.querySelector('.player-panel-' + activePlayer).classList.remove('.active'); 
-        
-        diceImg.style.display = 'none'; 
-    }else{
-        nextPlayer(); 
+    if(gamePlay){
+        scores[activePlayer] += currentScore; 
+    
+        document.querySelector('.score-' + activePlayer).textContent = scores[activePlayer]; 
+    
+        if(scores[activePlayer] >= winPoint){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.player-panel-' + activePlayer).classList.add('winner'); 
+            document.querySelector('.player-panel-' + activePlayer).classList.remove('active'); 
+            diceImg.style.display = 'none'; 
+            gamePlay = false; 
+        }else{
+            nextPlayer(); 
+        }
     }
 
 }); 
+
+btnNew.addEventListener('click', init); 
 
 function nextPlayer(){
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
@@ -71,4 +66,29 @@ function nextPlayer(){
     playerPan1.classList.toggle('active');
 
     // diceImg.style.display = 'none'; 
+}
+
+function init(){
+    gamePlay = true;
+    activePlayer = 0; 
+    currentScore = 0; 
+    winPoint = 20;
+    scores = [0,0]; 
+
+    document.querySelector('.score-0').textContent = '0';
+    document.querySelector('.score-1').textContent = '0';
+    document.querySelector('#current-0').textContent = '0';
+    document.querySelector('#current-1').textContent = '0';
+    diceImg.style.display = 'none'; 
+
+
+    
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+    playerPan0.classList.remove('winner'); 
+    playerPan1.classList.remove('winner');
+    playerPan0.classList.remove('active'); 
+    playerPan1.classList.remove('active');
+    playerPan0.classList.add('active');
+
 }
